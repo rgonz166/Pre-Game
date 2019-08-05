bf42bfa3b1eb3bfa4d751b331aa16df134224a26
Raul Gonzalez <rgonz166@gmail.com>
2019-08-04 22:17:29 -0700

    added yelp.html and working code to get information from yelp

--- assets/javascript/yelp.js
+++ assets/javascript/yelp.js
@@ -1,17 +1,50 @@
 // Yelp api
 
-var longitude = 32.7157;
-var latitude = 117.1611;
-var yelpClientID = 'XeMH_vKd8Mm-rmj3QWBF5Q';
-var yelpApiKey = 'ssPVBtHyaqwtWyJvqHRW24qlwpitFICGorpoIHxUJR-LoKpi0StKtRRdxGXek19oPHAfXelKVbUmrceV6hur0HXUsWxZTiJ7S3BRfa9Bp-YGfAWd_ftNzrDVe-1FXXYx';
-// var yelpQuery = 'https://api.yelp.com/v3/businesses/'+yelpClientID+'/search?';
-var yelpQuery = 'https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972';
 
-// ajax calls
-$.ajax({
-    method: 'GET',
-    url: yelpQuery
-}).then(function(res){
-    console.log(res);
-});
 
+$('.yelp-search').on('click',function(e){
+    e.preventDefault();
+    var b = $('.businesses');
+    var term = $('#input-result').val().trim();
+    var termString = '&term=' + term;
+
+    // var latitude = 32.715736;
+    // var longitude = -117.161087;
+    // var yelpClientID = 'XeMH_vKd8Mm-rmj3QWBF5Q';
+    var yelpApiKey = 'ssPVBtHyaqwtWyJvqHRW24qlwpitFICGorpoIHxUJR-LoKpi0StKtRRdxGXek19oPHAfXelKVbUmrceV6hur0HXUsWxZTiJ7S3BRfa9Bp-YGfAWd_ftNzrDVe-1FXXYx';
+    // // var yelpQuery = 'https://api.yelp.com/v3/businesses/search?latitude=' + latitude + '&longitude='+longitude;
+    // var yelpQuery = 'https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972';
+    
+    var myurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?latitude=32.715736&longitude=-117.161087&limit=10&sort_by=rating" + termString;
+
+
+        $.ajax({
+            url: myurl,
+            headers: {
+                'Authorization':'Bearer ' +yelpApiKey,
+            },
+            method: 'GET',
+            dataType: 'json',
+            success: function(data){
+                console.log(data);
+                for(var i =0;i<data.businesses.length;i++){
+                    var name = $('<p>');
+                    name.text(data.businesses[i].name);
+                    var pic = $('<img>');
+                    pic.attr('src', data.businesses[i].image_url);
+                    pic.attr('height', '200px');
+                    b.prepend(pic);
+                    b.prepend(name);
+                    console.log(data.businesses[i].image_url);
+                }
+            }
+        }); 
+})
+
+function loadData(){
+    var b = $('.businesses');
+    var address = $('<p>');
+    var pic = $('<img>');
+    // address
+    
+}
